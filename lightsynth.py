e9e3b51e3bbdb326d5e4d65b97d5d495fb8954d3
luca <lsomm@hotmail.com>
2023-03-18 17:30:12 +0100

    in synth?

--- lightsynth.py
+++ lightsynth.py
@@ -15,17 +15,7 @@ import numpy as np
 import time
 import atexit
 
-@atexit.register
-def say_goodbye():
-    print("exiting now, goodbye")
-    if pi_here:
-        pi.wave_tx_stop()
-    
-        pi.wave_clear()
-    
-        pi.write(pins[0],0)
-        pi.write(pins[1],0)
-        pi.write(pins[2],0)
+
 
 if os.uname()[4] == 'x86_64':
     pi_here = 0
@@ -183,5 +173,15 @@ with mido.open_input() as inport:
             
         
 
-       
+@atexit.register
+def say_goodbye():
+    print("exiting now, goodbye")
+    if pi_here:
+        pi.wave_tx_stop()
+    
+        pi.wave_clear()
+         
+        pi.write(pins[0],0)
+        pi.write(pins[1],0)
+        pi.write(pins[2],0)  
             
